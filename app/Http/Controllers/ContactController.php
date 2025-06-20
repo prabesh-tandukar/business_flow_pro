338eb1d7ec856305fb1609e9fe7dec0684036e2a
Prabesh <tandukarprabesh@gmail.com>
2025-06-20 22:44:43 +1200

    g

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,18 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use App\Models\Company;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ContactController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display contacts index page with data
      */
-    public function index()
+    public function index(Request $request): Response
     {
-        //
-    }
+        $query = Contact::with(['company', 'tags', 'owner'])->where('deleted_at', null);
 
+        //Search functionality
+        if ($request->filled('search')) {
+            $search = $request->search;
+            $query->where(function($q) use ($search) {
+                $q->whereRaw("CONCAT(first_name, ' ', last_name) LIKE ?", ["%{$search}%"])
+                ->orWhere('email', 'like', "%{$search}%")
+                ->orWhere('phone', 'like', "%{$search}%")
+                ->orWhere('mobile', 'like', "%{$search}%")
+                ->orWhereHas('company', function($companyQuery) use ($search){
+                    $companyQuery->where('name', 'like', "%{$search}%");
+                });
+
+            });
+        }
+
+    }
     /**
      * Show the form for creating a new resource.
      */
